88e53fbb5fdbc962ef6c014710301d5523815b2f
Siva <siva.ping.here@gmail.com>
2024-12-10 10:27:47 +0530

    Initial Commit

---
+++ ConsumePod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsumePod
+//
+//  Created by Sivasubramaniam on 10/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ConsumePodTests/ConsumePodTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConsumePodTests.swift
+//  ConsumePodTests
+//
+//  Created by Sivasubramaniam on 10/12/24.
+//
+
+import Testing
+@testable import ConsumePod
+
+struct ConsumePodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
